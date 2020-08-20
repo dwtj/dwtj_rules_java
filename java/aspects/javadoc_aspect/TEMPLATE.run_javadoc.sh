fa90bca0975e9237ab007a7d217096c12362ae10
David Johnston <dwtj@dwtj.me>
2020-08-20 15:34:27 -0700

    Make Javadoc aspect print log if exit is not zero

--- java/aspects/javadoc_aspect/TEMPLATE.run_javadoc.sh
+++ java/aspects/javadoc_aspect/TEMPLATE.run_javadoc.sh
@@ -9,23 +9,27 @@
 # - HTML_ARCHIVE_FILE (expected to be a `.tar.gz` file): {HTML_ARCHIVE_FILE}
 # - JAVADOC_LINT_LOG_FILE: {JAVADOC_LINT_LOG_FILE}
 
-set -e
-
 mkdir -p "{HTML_TEMP_DIRECTORY}/javadoc"
 
-{
-    "{JAVADOC_EXECUTABLE}" \
-        "@{CLASS_PATH_ARGS_FILE}" \
-        -Xdoclint:all \
-        -private \
-        -d "{HTML_TEMP_DIRECTORY}/javadoc" \
-        "@{JAVA_SOURCE_ARGS_FILE}"
+"{JAVADOC_EXECUTABLE}" \
+    "@{CLASS_PATH_ARGS_FILE}" \
+    -Xdoclint:all \
+    -private \
+    -d "{HTML_TEMP_DIRECTORY}/javadoc" \
+    "@{JAVA_SOURCE_ARGS_FILE}" \
+        2>&1 > "{JAVADOC_LINT_LOG_FILE}"
+RETURN_CODE=$?
 
-    # TODO(dwtj): Consider making the TAR file creation process more robust
-    #  (e.g. consider how the `tar` executable ought to be found).
-    tar --create \
-        --gzip \
-        --file "{HTML_ARCHIVE_FILE}" \
-        --directory="{HTML_TEMP_DIRECTORY}" \
-        "javadoc"
-} > "{JAVADOC_LINT_LOG_FILE}" 2>&1
+if [ $RETURN_CODE -ne 0 ] ; then
+    cat "{JAVADOC_LINT_LOG_FILE}"
+    exit $RETURN_CODE
+fi
+
+# TODO(dwtj): Consider making the TAR file creation process more robust
+#  (e.g. consider how the `tar` executable ought to be found).
+set -e
+tar --create \
+    --gzip \
+    --file "{HTML_ARCHIVE_FILE}" \
+    --directory="{HTML_TEMP_DIRECTORY}" \
+    "javadoc"
