65e32988833f82d5f1e4fb289bf18f1975574920
David Johnston <dwtj@dwtj.me>
2020-10-06 11:56:33 -0700

    Add a test which links with native-image library

---
+++ test/workspaces/graalvm/smoke_test_graalvm_native_image_rules/pkg/hello_dynamic_test.cc
@@ -0,0 +1,7 @@
+#include "pkg/hello_dynamic.h"
+
+int main(int argc, char* argv[]) {
+    // TODO(dwtj): Figure out how to use the interface provided by the dynamic
+    //  headers.
+    return 0;
+}
---
+++ test/workspaces/graalvm/smoke_test_graalvm_native_image_rules/pkg/hello_test.cc
@@ -0,0 +1,5 @@
+#include "pkg/hello.h"
+
+int main(int argc, char* argv[]) {
+    return run_main(argc, argv);
+}
