5ad53791418d8a354bef6d913d8ec46df2200ff2
David Johnston <dwtj@dwtj.me>
2020-10-14 11:23:35 -0700

    Fix the name of a Java class in a test wkspc

--- test/workspaces/smoke_test_toolchain_overrides/st/MustBeCompiledWithGraal.java
+++ test/workspaces/smoke_test_toolchain_overrides/st/MustBeCompiledWithGraal.java
@@ -3,7 +3,7 @@ package st;
 import org.graalvm.nativeimage.IsolateThread;
 import org.graalvm.nativeimage.c.function.CEntryPoint;
 
-class MyTestApp {
+class MustBeCompiledWithGraal {
     @CEntryPoint
     public static void myMethod(IsolateThread isolateThread) {
         System.out.println("Hello, world!");
